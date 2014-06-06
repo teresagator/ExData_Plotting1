97e9115d7a07ed4224bf1bbeb2c73681fc3d5aff
teresagator <tnieten@acm.org>
2014-06-06 15:41:13 -0400

    Fixed to read full data file instead of partial data file

--- plot4.R
+++ plot4.R
@@ -1,7 +1,7 @@
 plot4 <- function() {
-  energy_2007 <- read.table("household_power_consumption_2007.txt", header=TRUE, sep=";", na.strings="?")
-  energy_2007$Date <- as.Date(energy_2007$Date, "%d/%m/%Y")
-  Feb <- energy_2007[which(energy_2007$Date >= "2007-02-01" & energy_2007$Date <= "2007-02-02"),]
+  energy <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
+  energy$Date <- as.Date(energy$Date, "%d/%m/%Y")
+  Feb <- energy[which(energy$Date >= "2007-02-01" & energy$Date <= "2007-02-02"),]
   Feb$DateTime <- paste(Feb$Date, Feb$Time)
   Feb$DateTime <- strptime(Feb$DateTime, format="%Y-%m-%d %H:%M:%S")
   
